50eb8fad99a75470077f2cd9881ea8a13335c72f
harshad <harshad@ubuntu20.4>
2020-10-17 01:34:13 +0530

    Basic django tutorial

---
+++ adoptions/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from .models import Pet, Vaccine
+
+# Create your views here.
+def home(request):
+    pets = Pet.objects.all()
+    return render(request,'home.html',{'pets':pets})
+
+def pet_detail(request,pet_id):
+    try:
+        pet = Pet.objects.get(id = pet_id)
+    except Pet.DoesNotExist as e:
+        raise Http404("Pet not found")
+    return render(request,'pet_detail.html',{'pet':pet})
+        
+    
